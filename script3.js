4266aca9e55f5b560aa5153fb002fc66d565760a
Moorpho <grib.sofia.ua@gmail.com>
2024-07-29 16:07:18 +0100

    Add files via upload

--- script3.js
+++ script3.js
@@ -1,7 +1,34 @@
-function showFirstMessage(text){
-    alert(text);
-    let num = 20;
+//let num = 20;
+
+//function showFirstMessage(text){
+ //   alert(text);
+   // console.log(num);
+//}
+
+//showFirstMessage("Hello World! ");
+// console.log(num);
+
+//let calc = function calc(a,b) {
+  //  return (a+b);
+//}
+
+let calc = (a,b) => a+b
+
+console.log(calc(3,4));
+
+console.log(calc(8,4));
+
+function retVar() {
+    let num = 50;
+    return num;
 }
 
-showFirstMessage("Hello World! ");
-console.log(num);
+let anotherNum = retVar();
+console.log(anotherNum );
+
+let str = "test";
+console.log(str.toUpperCase( )); 
+console.log(str.toLowerCase()); 
+
+let twelve = "12.2 ";
+console.log(Meth.round(twelve));
